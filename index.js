bc118cb23369f1f0c76b420d99880031d8c1b629
akshitatom90 <138872947+akshitatom90@users.noreply.github.com>
2023-10-18 20:58:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const share = document.querySelector(".share-info");
+const shareicon = document.querySelector(".share-img");
+
+
+shareicon.addEventListener("click",function(){
+    share.classList.toggle('x')
+})
+
